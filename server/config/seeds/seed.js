f5501e36c811df59192e7719ef544ba8fa96517a
Andrew Carroll <andrewc411@yahoo.com>
2022-02-12 11:43:35 -0500

    added seed and utils

--- server/config/seeds/seed.js
+++ server/config/seeds/seed.js
@@ -1,15 +1,55 @@
-const db = require('../config/connection');
-const { Profile } = require('../models');
-const profileSeeds = require('./profileSeeds.json');
+const connection = require('../config/connection.js');
+const { usernames, emails, thoughts } = require('./data');
+const { User, Thought } = require('../models')
 
-db.once('open', async () => {
-  try {
-    await Profile.deleteMany({});
-    await Profile.create(profileSeeds);
 
-    console.log('all done!');
+const user = [];
+const userThought = [];
+insertUsers();
+insertThoughts();
+
+
+function insertUsers() {
+    for (let i = 0; i < usernames.length; i++) {
+        const userObj = {
+            username: usernames[i],
+            email: emails[i]
+        }
+        user.push(userObj)
+    }
+};
+
+function insertThoughts() {
+    for (let i = 0; i < thoughts.length; i++) {
+        const thought = thoughts[i];
+        const thoughtObj = {
+            thoughtText: thought.thoughtText,
+            username: thought.username,
+        }
+        userThought.push(thoughtObj)
+    }
+};
+
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+
+
+    await User.deleteMany({});
+
+
+    await Thought.deleteMany({});
+
+    await User.insertMany(user);
+
+    await Thought.insertMany(userThought);
+
+    console.table(user);
+    console.table(userThought);
+    console.info('Seeding complete! 🌱');
     process.exit(0);
-  } catch (err) {
-    throw err;
-  }
+
+
 });
