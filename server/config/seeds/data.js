522c139b31a62705fd6ee579661fccd004bfcfc1
Andrew Carroll <andrewc411@yahoo.com>
2022-02-12 11:57:52 -0500

    added seed and utils

---
+++ server/config/seeds/data.js
@@ -0,0 +1,43 @@
+const usernames = [
+    'andrewmagic',
+    'tinytim',
+    'omicron',
+    'moana',
+    'deadlysnowman'
+];
+
+const emails = [
+    'andrewmagic@email.com',
+    'tinytim@email.com',
+    'omicron@email.com',
+    'moana@email.com',
+    'deadlysnowman@email.com'
+];
+
+
+const thoughts = [
+    {
+        thoughtText: 'This is me thinking of thoughts',
+        username: 'andrewmagic'
+    },
+    {
+        thoughtText: 'Wow that was crazy',
+        username: 'tinytim'
+    },
+    {
+        thoughtText:
+            'Maybe I will do it tomorrow',
+        username: 'omicron'
+    },
+    {
+        thoughtText: 'Mmmmm that is good coffee',
+        username: 'moana'
+    },
+    {
+        thoughtText: 'Kill all humans',
+        username: 'deadlysnowman'
+    }
+
+]
+
+module.exports = { usernames, emails, thoughts };
