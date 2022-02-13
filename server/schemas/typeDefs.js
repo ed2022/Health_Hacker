0af211c7c4a2fc6785644fcddea4615346947f6d
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-02-12 23:09:24 -0500

    typedefs prototype

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,47 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        id: ID
+        username: String
+        email: String
+        password: String
+        profile: Profile
+    }
+
+    type Profile {
+        id: ID
+        user: User
+        calendar: Calendar
+
+    }
+    
+    type Calendar {
+
+    }
+
+    type Workout {
+    
+    }
+
+    type Auth {
+        token: ID!
+        user: User!
+    }
+
+    type Query {
+    
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        updateProfile(): Profile
+        updateCalendar(): Calendar
+        updateWorkout(): Workout
+
+
+    }
+    `;
+
+module.exports = typeDefs;
