c7a937fdb77ba5ca3fc68bb77e45387de54bf47e
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-02-13 19:29:26 -0500

    index commit

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,9 @@
 const User = require('./User');
+const Post = require('./Post');
+const Profile = require('./Profile');
+const Recipe = require('./Recipe');
+const Calendar = require('./Calendar');
+const Workout = require('./Workout');
 
-module.exports = { User };
+
+module.exports = { User, Post, Profile, Recipe, Calendar, Workout };
