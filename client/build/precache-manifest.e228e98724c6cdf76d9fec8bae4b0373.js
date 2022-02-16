48a35a4a304a0844beac8e34a8dbf346a1aa05db
Nova <eldanadesalgne@gmail.com>
2022-02-16 13:41:55 -0500

    trying a pull

---
+++ client/build/precache-manifest.e228e98724c6cdf76d9fec8bae4b0373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4fadff3244766d620a35546573f349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f0750727ce5428f19c",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "102caa35015a2fbe341e",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "57f0750727ce5428f19c",
+    "url": "/static/js/2.57c3235a.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.57c3235a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102caa35015a2fbe341e",
+    "url": "/static/js/main.ffe67015.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
